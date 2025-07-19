419a3ef031f5b14913b4d941a1d68c19d14980f8
acrdiaz <acrdiaz@msn.com>
2025-07-18 23:50:35 -0400

    calc cli challenge

--- ac_calc_cli/src/main.rs
+++ ac_calc_cli/src/main.rs
@@ -6,11 +6,14 @@ fn main() {
         let opcion = leer_opcion();
 
         match opcion.as_str() {
-            "1" => realizar_operacion(|a, b| a + b),
-            "2" => realizar_operacion(|a, b| a - b),
-            "3" => realizar_operacion(|a, b| a * b),
+            "1" => realizar_operacion(|a, b| Ok(a + b)),
+            "2" => realizar_operacion(|a, b| Ok(a - b)),
+            "3" => realizar_operacion(|a, b| Ok(a * b)),
             "4" => realizar_operacion(dividir),
-            "5" => {
+            "5" => realizar_operacion(|a, b| Ok(a.powf(b))),
+            "6" => realizar_operacion_unaria(calcular_raiz),
+            "7" => realizar_porcentaje(calcular_porcentaje),
+            "8" => {
                 println!("Saliendo de la calc, Gracias!");
                 break;
             }
@@ -25,7 +28,10 @@ fn mostrar_menu() {
     println!("2. Restar");
     println!("3. Multiplicar");
     println!("4. Dividir");
-    println!("5. Salir");
+    println!("5. Elevar");
+    println!("6. RaizQ");
+    println!("7. Porcentaje");
+    println!("8. Salir");
     print!("Elige una opción: ");
     // Asegura que se imprima inmediatamente
     io::stdout().flush().unwrap();
@@ -52,20 +58,65 @@ fn leer_numero(mensaje: &str) -> f64 {
     }
 }
 
+fn realizar_operacion_unaria<F>(operacion: F)
+where
+    F: Fn(f64) -> Result<f64, String>
+{
+    let a = leer_numero("Ingresa el número: ");
+
+    match operacion(a) {
+        Ok(resultado) => println!("El resultado es: {}\n", resultado),
+        Err(e) => print!("Error: {}\n", e),
+    }
+    // let resultado = operacion(a);
+}
+
 fn realizar_operacion<F>(operacion: F)
 where
-    F: Fn(f64, f64) -> f64,
+    F: Fn(f64, f64) -> Result<f64, String>
 {
     let a = leer_numero("Ingresa el primer número: ");
     let b = leer_numero("Ingresa el segundo número: ");
 
-    let resultado = operacion(a, b);
-    println!("El resultado es: {}\n", resultado);
+    match operacion(a, b) {
+        Ok(resultado) => println!("El resultado es: {}\n", resultado),
+        Err(e) => println!("Error: {}\n", e),
+    }
+    // let resultado = operacion(a, b);
 }
 
-fn dividir(a: f64, b: f64) -> f64 {
-    if b == 0.0 {
-        panic!("División por cero detectada!");
+fn realizar_porcentaje<F>(operacion: F)
+where
+    F: Fn(f64, f64) -> Result<f64, String>
+{
+    let a = leer_numero("Ingresa el primer número: ");
+    let b = leer_numero("Ingresa el segundo número: ");
+
+    match operacion(a, b) {
+        Ok(resultado) => println!("El resultado es: {}%\n", resultado),
+        Err(e) => println!("Error: {}\n", e),
     }
-    a / b
+    // let resultado = operacion(a, b);
+}
+
+fn dividir(a: f64, b: f64) -> Result<f64, String> {
+    if b == 0.0 {
+        return Err("División por cero detectada.".to_string());
+    }
+    Ok(a / b)
+}
+
+fn calcular_raiz(a: f64) -> Result<f64, String> {
+    if a < 0.0 {
+        return Err("No se puede calcular raiz cuadrada de un número negativo.".to_string());
+    }
+    Ok(a.sqrt())
+}
+
+fn calcular_porcentaje(a: f64, b: f64) -> Result<f64, String> {
+    if b == 0.0 {
+        return Err("No se puede calcular porcentaje si el segundo número es 0.".to_string());
+    }
+
+    Ok((a / b) * 100.0)
 }
